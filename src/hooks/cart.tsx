ccbdb554dcbbd628bafbff67330d30f0a7a5b791
Matheus Zanin <matt_z6@hotmail.com>
2020-05-01 14:30:34 -0300

    feat: Add, increment and decrement products on cart

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -16,37 +16,94 @@ interface Product {
   quantity: number;
 }
 
+interface AddProduct {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity?: number;
+}
+
 interface CartContext {
   products: Product[];
-  addToCart(item: Product): void;
+  addToCart(item: AddProduct): void;
   increment(id: string): void;
   decrement(id: string): void;
 }
 
 const CartContext = createContext<CartContext | null>(null);
 
+const ITEMS_KEY = '@GoMarketplace:products';
+
 const CartProvider: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const items = await AsyncStorage.getItem(ITEMS_KEY);
+
+      if (items) {
+        setProducts(JSON.parse(items));
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async (item: AddProduct) => {
+      const exists = products.some(x => x.id === item.id);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      let updatedProducts: Product[] = [];
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (exists) {
+        updatedProducts = products.map(x =>
+          x.id === item.id ? { ...x, quantity: x.quantity + 1 } : x,
+        );
+      } else {
+        updatedProducts = [...products, { ...item, quantity: 1 }];
+      }
+
+      setProducts(updatedProducts);
+
+      await AsyncStorage.setItem(ITEMS_KEY, JSON.stringify(updatedProducts));
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async (id: string) => {
+      const updatedProducts = products.map(x =>
+        x.id === id ? { ...x, quantity: x.quantity + 1 } : x,
+      );
+
+      setProducts(updatedProducts);
+
+      await AsyncStorage.setItem(ITEMS_KEY, JSON.stringify(updatedProducts));
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async (id: string) => {
+      const mustRemove = products.some(x => x.id === id && x.quantity === 1);
+
+      let updatedProducts: Product[];
+
+      if (mustRemove) {
+        updatedProducts = products.filter(x => x.id !== id);
+      } else {
+        updatedProducts = products.map(x =>
+          x.id === id ? { ...x, quantity: x.quantity - 1 } : x,
+        );
+      }
+
+      setProducts(updatedProducts);
+
+      await AsyncStorage.setItem(ITEMS_KEY, JSON.stringify(updatedProducts));
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { View, Image } from 'react-native';
+import { View, Image, Alert } from 'react-native';
 
 import formatValue from '../../utils/formatValue';
 import { useCart } from '../../hooks/cart';
@@ -35,14 +35,20 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      try {
+        const { data } = await api.get('/products');
+
+        setProducts(data);
+      } catch (error) {
+        Alert.alert('Ops...', 'Não foi possível buscar os produtos');
+      }
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
@@ -50,11 +56,9 @@ const Dashboard: React.FC = () => {
       <ProductContainer>
         <ProductList
           data={products}
-          keyExtractor={item => item.id}
+          keyExtractor={(item: Product) => item.id}
           ListFooterComponent={<View />}
-          ListFooterComponentStyle={{
-            height: 80,
-          }}
+          ListFooterComponentStyle={{ height: 80 }}
           renderItem={({ item }) => (
             <Product>
               <ProductImage source={{ uri: item.image_url }} />
